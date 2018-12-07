da0d3b8215798a58fa478a011f456107e3226401
Maria Öhrman <maria.ohrman@gu.se>
2018-12-07 14:05:26 +0100

    layout for subtypes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,62 @@
 <template>
   <div class="row">
-    <a @click="logout()">Logga ut</a>
     <div class="col-12">
-      <div class="row"  v-for="subtype in published">
-        {{subtype}}
+      <div class="row">
+        <div class="col-auto">
+          <h1>Hantera sakområden</h1>
+        </div>
+        <div class="col-auto ml-auto">
+          <span class="logout" @click="logout()">Logga ut</span>
+        </div>
       </div>
-      <div class="row"  v-for="subtype in unpublished">
-        {{subtype}}
+
+      <div class="row">
+        <div class="col-auto">
+          <div class="row align-items-center subtype-row" v-for="subtype in published">
+            <div class="col-12">
+              {{subtype}}
+            </div>
+          </div>
+          <div class="row align-items-center subtype-row" v-for="subtype in unpublished">
+            <div class="col-12">
+              {{subtype}}
+            </div>
+          </div>
+        </div>
+        <div class="col-auto">
+          <div class="row subtype-row" v-for="subtype in published">
+            <div class="col-12">
+              <button class="btn btn-danger">Avpublicera</button>
+            </div>
+          </div>
+          <div class="row subtype-row" v-for="subtype in unpublished">
+            <div class="col-12">
+              <button class="btn btn-primary">Publicera</button>
+            </div>
+          </div>
+        </div>
       </div>
+      
+      <!-- <div class="row">
+        <div class="col-12">
+          <div class="row align-items-center subtype-row"  v-for="subtype in published">
+            <div class="col-auto">
+              {{subtype}}
+            </div>
+            <div class="col-auto">
+              <button class="btn btn-primary">Avpublicera</button>
+            </div>
+          </div>
+          <div class="row align-items-center subtype-row"  v-for="subtype in unpublished">
+            <div class="col-auto">
+              {{subtype}}
+            </div>
+            <div class="col-auto">
+              <button class="btn btn-primary">Publicera</button>
+            </div>
+          </div>
+        </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -30,12 +79,24 @@ export default {
     }
   },
   created () {
-    const subtypes = backend.getSubtypes()
-    this.published = subtypes.published
-    this.unpublished = subtypes.unpublished
+    const subtypes = backend.getSubtypes().then((subtypes) => {
+      this.published = subtypes.published
+      this.unpublished = subtypes.unpublished
+    })
   }
 }
 </script>
 
 <style scoped>
+.logout {
+  cursor: pointer;
+}
+.logout:hover {
+  text-decoration: underline;
+}
+.subtype-row {
+  height: 50px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
 </style>
