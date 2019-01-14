4c2f5c1a1e722cc0356878cae13fda78832d89aa
Maria Öhrman <maria.ohrman@gu.se>
2019-01-14 17:06:23 +0100

    new backend location

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://ws.spraakbanken.gu.se/ws/tolkordlistor'
+  baseURL: 'https://ws.spraakbanken.gu.se/ws/minoritetsordlistan'
 })
 
 const karpInstance = axios.create({
